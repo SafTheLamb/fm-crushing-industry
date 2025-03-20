6b92be6c5fcc503f6fa1e3da19f3cde109e9ae13
Nicholas Gower <nicholasgower01@gmail.com>
2025-03-19 19:02:59 -0700

    Added custom locale for Muluna's Crusher 2

--- compat/planet-muluna.lua
+++ compat/planet-muluna.lua
@@ -7,6 +7,7 @@ if mods["planet-muluna"] then
         frep.scale_ingredient("crusher-2", "tungsten-plate", {amount=5})
         frep.replace_ingredient("crusher-2", "uranium-238", {type="item", name="quality-module-3", amount=4})
         data.raw["assembling-machine"]["crusher"].effect_receiver = {base_effect={quality=2, productivity=0.25}}
+        data.raw["assembling-machine"]["crusher-2"].localised_name = {"entity-name.crusher-n","2"}
     end
 end
 
