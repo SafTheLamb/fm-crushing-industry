ad6969c3ec50f0040a1157345224302d0b0747f8
sapphiriza <sapphiriza@gmail.com>
2025-04-24 13:05:48 -0700

    Short term fix for Nuclear science pack

--- data.lua
+++ data.lua
@@ -16,3 +16,7 @@ require("prototypes.compat.any-planet-start")
 require("prototypes.compat.base")
 require("prototypes.compat.space-age")
 require("prototypes.compat.bz-ores")
+
+-- NOTE: short term fix that will be obsolete with the new API
+CrushingIndustry.concrete_recipes["nuclear-science-pack"] = {ignore=true}
+CrushingIndustry.concrete_recipes["nuclear-science-pack-from-plutonium"] = {ignore=true}
