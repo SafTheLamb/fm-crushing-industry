3afa6930a8fca37deff8becd369dc1bc001937de
sapphiriza <sapphiriza@gmail.com>
2025-05-10 12:48:46 -0700

    Large overhaul to compatibility with BZ Tin and Carbon

--- prototypes/compat/bz-ores-updates.lua
+++ prototypes/compat/bz-ores-updates.lua
@@ -7,21 +7,40 @@ if data.raw.furnace["basic-crusher"] then
 end
 
 if mods["bzlead"] and mods["space-age"] then
-  data.raw.recipe["alternative-metallic-asteroid-crushing"].hidden = true
+  if data.raw.recipe["alternative-metallic-asteroid-crushing"] then
+    data.raw.recipe["alternative-metallic-asteroid-crushing"].hidden = true
+  end
+  
+  frep.reorder_result("advanced-metallic-asteroid-crushing", "lead-ore", 1)
   if settings.startup["crushing-industry-byproducts"].value then
     frep.remove_result("metallic-asteroid-crushing", "stone")
-    -- frep.remove_result("advanced-metallic-asteroid-crushing", "stone")
+    if settings.startup["crushing-industry-ore"].value then
+      frep.replace_result("advanced-metallic-asteroid-crushing", "crushed-iron-ore", "crushed-lead-ore")
+    end
   end
   frep.add_result("metallic-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("iron-ore", CrushingIndustry.FREQUENT_BYPRODUCT, 10, true), false, 2)
   -- frep.add_result("advanced-metallic-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("iron-ore", CrushingIndustry.COMMON_BYPRODUCT, 5, true), false, 3)
 end
 
 if mods["bztin"] and mods["cupric-asteroids"] then
-  data.raw.recipe["alternative-carbonic-asteroid-crushing"].hidden = true
-  if settings.startup["crushing-industry-byproducts"].value then
-    frep.remove_result("carbonic-asteroid-crushing", "coal")
-    frep.remove_result("advanced-carbonic-asteroid-crushing", "coal")
+  if data.raw.recipe["cupric-asteroid-crushing-tin"] then
+    data.raw.recipe["cupric-asteroid-crushing-tin"].hidden = true
   end
-  frep.add_result("carbonic-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("tin-ore", CrushingIndustry.FREQUENT_BYPRODUCT, 5, true), false, 2)
-  frep.add_result("advanced-carbonic-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("tin-ore", CrushingIndustry.COMMON_BYPRODUCT, 3, true), false, 3)
+
+  if settings.startup["crushing-industry-byproducts"].value then
+    frep.remove_result("cupric-asteroid-crushing", "stone")
+    frep.remove_result("advanced-cupric-asteroid-crushing", "stone")
+  end
+  frep.add_result("cupric-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("tin-ore", CrushingIndustry.FREQUENT_BYPRODUCT, 5, true), false, 2)
+  frep.add_result("advanced-cupric-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("tin-ore", CrushingIndustry.COMMON_BYPRODUCT, 3, true), false, 3)
+end
+
+if mods["bzcarbon"] and mods["space-age"] then
+  if settings.startup["bzcarbon-enable-flake-graphite"].value then
+    if settings.startup["crushing-industry-byproducts"].value then
+      frep.remove_result("carbonic-asteroid-crushing", "coal")
+      frep.replace_result("advanced-carbonic-asteroid-crushing", "coal", "flake-graphite")
+    end
+    frep.add_result("carbonic-asteroid-crushing", CrushingIndustry.make_crushing_byproduct("flake-graphite", CrushingIndustry.FREQUENT_BYPRODUCT, 3, true), false, 2)
+  end
 end
