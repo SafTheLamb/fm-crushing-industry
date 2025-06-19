8654a4d9fb06e47651766050cd305c7fcede7c54
SafTheLamb <safthelamb@gmail.com>
2025-06-18 23:08:58 -0700

    Simplify concrete mix barrel modifications

--- prototypes/item-updates.lua
+++ prototypes/item-updates.lua
@@ -1,16 +1,12 @@
 local multispoil = require("__multispoil__.api")
 
 if settings.startup["crushing-industry-concrete-mix"].value and settings.startup["crushing-industry-concrete-spoil-amount"].value > 0 then
-  local concrete_mix_barrel = data.raw.item["concrete-mix-barrel"]
-  if feature_flags["spoiling"] then
-    if concrete_mix_barrel then
-      concrete_mix_barrel.spoil_ticks = 0.26*hour
-      concrete_mix_barrel.spoil_result = "concrete"
-      if settings.startup["crushing-industry-concrete-spoil-amount"].value == 1 then
-        concrete_mix_barrel.spoil_result = "concrete"
-      else
-        concrete_mix_barrel.spoil_to_trigger_result = multispoil.create_spoil_trigger({["concrete"] = settings.startup["crushing-industry-concrete-spoil-amount"].value - 1})
-      end
-    end
-  end
+	local concrete_mix_barrel = data.raw.item["concrete-mix-barrel"]
+	if concrete_mix_barrel then
+		concrete_mix_barrel.spoil_ticks = 0.26*hour
+		concrete_mix_barrel.spoil_result = "concrete"
+		if settings.startup["crushing-industry-concrete-spoil-amount"].value > 1 then
+			concrete_mix_barrel.spoil_to_trigger_result = multispoil.create_spoil_trigger({["concrete"] = settings.startup["crushing-industry-concrete-spoil-amount"].value - 1})
+		end
+	end
 end
