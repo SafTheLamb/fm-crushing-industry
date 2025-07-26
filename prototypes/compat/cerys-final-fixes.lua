5a3c541d379011c106c5f910be037e2ec59c1970
SafTheLamb <safthelamb@gmail.com>
2025-07-26 15:24:32 -0700

    Fix load error

--- prototypes/compat/cerys-final-fixes.lua
+++ prototypes/compat/cerys-final-fixes.lua
@@ -2,6 +2,6 @@ if mods["Cerys-Moon-of-Fulgora"] then
 	PlanetsLib.remove_surface_condition(data.raw.recipe["carbonic-asteroid-crushing"], "cerys-ambient-radiation")
 	PlanetsLib.remove_surface_condition(data.raw.recipe["advanced-carbonic-asteroid-crushing"], "cerys-ambient-radiation")
 	if settings.startup["crushing-industry-ore"].value then
-		PlanetsLib.remove_surface_condition(data.raw.recipe["tungsten-ore-crushing"], "cerys-ambient-radiation")
+		PlanetsLib.remove_surface_condition(data.raw.recipe["crushed-tungsten-ore"], "cerys-ambient-radiation")
 	end
 end
