a70511a32964df152cfb4ae8e4a7aa5da4a5ad09
SafTheLamb <safthelamb@gmail.com>
2025-07-27 13:05:17 -0700

    Fix cerys fulgoran crusher being unable to crush asteroids

--- prototypes/compat/cerys-final-fixes.lua
+++ prototypes/compat/cerys-final-fixes.lua
@@ -1,4 +1,11 @@
 if mods["Cerys-Moon-of-Fulgora"] then
+	local fulgoran_crusher = data.raw["assembling-machine"]["cerys-fulgoran-crusher"]
+	if fulgoran_crusher then
+		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing")
+		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing-or-crafting")
+		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing-or-hand-crafting")
+		table.insert(fulgoran_crusher.crafting_categories, "crushing-or-crafting")
+	end
 	PlanetsLib.remove_surface_condition(data.raw.recipe["carbonic-asteroid-crushing"], "cerys-ambient-radiation")
 	PlanetsLib.remove_surface_condition(data.raw.recipe["advanced-carbonic-asteroid-crushing"], "cerys-ambient-radiation")
 	if settings.startup["crushing-industry-ore"].value then
--- prototypes/compat/cerys.lua
+++ prototypes/compat/cerys.lua
@@ -1,10 +1,6 @@
 if mods["Cerys-Moon-of-Fulgora"] then
 	local fulgoran_crusher = data.raw["assembling-machine"]["cerys-fulgoran-crusher"]
 	if fulgoran_crusher then
-		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing")
-		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing-or-crafting")
-		table.insert(fulgoran_crusher.crafting_categories, "basic-crushing-or-hand-crafting")
-		table.insert(fulgoran_crusher.crafting_categories, "crushing-or-crafting")
 		if settings.startup["crushing-industry-space-crusher"].value then
 			fulgoran_crusher.crafting_speed = 1.5 * fulgoran_crusher.crafting_speed
 			fulgoran_crusher.effect_receiver = {base_effect={quality=1.25, productivity=0.25}}
