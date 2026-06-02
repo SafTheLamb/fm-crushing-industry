611b721fd114491f638989d17c2a42341fc2d82d
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-02 13:51:27 -0700

    compat: Fixed Muluna compatibility to take into account "Space crusher quality" setting when changing settings of Muluna's "Space crusher 2." https://github.com/nicholasgower/planet-muluna/issues/367

--- prototypes/compat/planet-muluna-updates.lua
+++ prototypes/compat/planet-muluna-updates.lua
@@ -8,7 +8,15 @@ if mods["planet-muluna"] then
     frep.scale_ingredient("crusher-2", "aluminum-plate", {amount=5})
     frep.scale_ingredient("crusher-2", "tungsten-plate", {amount=5})
     frep.replace_ingredient("crusher-2", "uranium-238", {type="item", name="quality-module-3", amount=4})
-    data.raw["assembling-machine"]["crusher-2"].effect_receiver = {base_effect={quality=2, productivity=0.25}}
+    
+    if settings.startup["crushing-industry-space-crusher-quality"].value then
+      data.raw["assembling-machine"]["crusher-2"].effect_receiver = {base_effect={quality=1.25, productivity=0.25}}
+      data.raw["assembling-machine"]["crusher-2"].energy_usage = "2160kW"
+    else
+      data.raw["assembling-machine"]["crusher-2"].effect_receiver = {base_effect={productivity=0.25}}
+      data.raw["assembling-machine"]["crusher-2"].energy_usage = "1620kW"
+    end
+
     data.raw["assembling-machine"]["crusher-2"].localised_name = {"entity-name.space-crusher-n", "2"}
   end
 
